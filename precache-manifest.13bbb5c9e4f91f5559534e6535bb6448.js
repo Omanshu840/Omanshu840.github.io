7232061b85477449a3aaaaf70bbfa38c1cc93beb
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-05-15 20:57:45 +0530

    Updates

---
+++ precache-manifest.13bbb5c9e4f91f5559534e6535bb6448.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27a7af67b72ddc6f1b570b14cef061a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eccc6db407b6d5265741",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a392e95225d6169600f3",
+    "url": "/static/css/main.9651b3ff.chunk.css"
+  },
+  {
+    "revision": "eccc6db407b6d5265741",
+    "url": "/static/js/2.2f3c3f39.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/static/js/2.2f3c3f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a392e95225d6169600f3",
+    "url": "/static/js/main.b6b87d6b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "56d6f1d3f777df56e432c10ca0786db7",
+    "url": "/static/media/activity1.56d6f1d3.png"
+  },
+  {
+    "revision": "36c75489c553e1cafeaf59553b5b7bf5",
+    "url": "/static/media/activity2.36c75489.png"
+  },
+  {
+    "revision": "7b27900ab6972750ba380c8bd27950df",
+    "url": "/static/media/project1.7b27900a.png"
+  },
+  {
+    "revision": "ba5b750d2266893d0997939bc243b850",
+    "url": "/static/media/project2.ba5b750d.png"
+  },
+  {
+    "revision": "7ce61cacb0b9fc906b0bc93f74b8dd9e",
+    "url": "/static/media/project3.7ce61cac.png"
+  },
+  {
+    "revision": "c6d950ff40367f2dd8e416e29c5d6132",
+    "url": "/static/media/project4.c6d950ff.png"
+  },
+  {
+    "revision": "afb3d2b6bb4e01bf60dc8dfbcad02ce1",
+    "url": "/static/media/project5.afb3d2b6.png"
+  },
+  {
+    "revision": "a1549afe37d6594109f88112cc6b702a",
+    "url": "/static/media/project6.a1549afe.png"
+  },
+  {
+    "revision": "f7f21196fef4d5556719006975aaad3c",
+    "url": "/static/media/project7.f7f21196.png"
+  }
+]);
