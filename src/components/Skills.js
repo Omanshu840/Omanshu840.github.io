112a41ca23585fd28c384743f9cdfe133920f67e
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-05-15 19:57:52 +0530

    Updating skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Fade from 'react-reveal/Fade';
+import skills from "../constants/skills";
 
 class Skills extends React.Component {
 
     render() {
 
-        const data = {
-            development: [["C++", "95"], ["Python", "66"], ["Java", "75"], ["HTML", "95"], ["CSS", "90"], ["Javascript", "66"], ["SQL", "66"]],
-            frameworks: [["Django", "66"], ["Flask", "80"], ["React", "85"], ["Node", "75"]],
-        }
+        const data = skills;
 
         return (
             <div id="skills" className="section-odd skills">
@@ -21,36 +19,20 @@ class Skills extends React.Component {
                 <div className="container">
                     <Fade top>
                         <div className="row">
-                            <div className="col-12 col-md-5 skill-subset">
-                                <h3>Development</h3>
-                                    {data.development.map(item => {
-                                        return (
-                                            <div className="skill">
-                                                <div className="col-12">
-                                                    {item[0]}
-                                                </div>
-                                                <div className="col-12">
-                                                    <progress id="file" value={item[1]} max="100"></progress>
-                                                </div>
-                                            </div>
-                                        )
-                                    })}
-                            </div>
-                            <div className="col-12 col-md-5 skill-subset">
-                                <h3>Frameworks</h3>
-                                    {data.frameworks.map(item => {
-                                        return (
-                                            <div className="skill">
-                                                <div className="col-12">
-                                                    {item[0]}
-                                                </div>
-                                                <div className="col-12">
-                                                    <progress id="file" value={item[1]} max="100"></progress>
-                                                </div>
-                                            </div>
-                                        )
-                                    })}
-                            </div>
+                            {data.map(item => {
+                                return (
+                                    <div className="col-12 col-md-5 skill-subset">
+                                        <h3>{item.name}</h3>
+                                        <div className="project-stack">
+                                            {item.skill.map(item => {
+                                                return (
+                                                    <span className="stack-item">{item}</span>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
                     </Fade>
                 </div>
---
+++ src/constants/skills.js
@@ -0,0 +1,24 @@
+const skills = [
+    {
+        id: 1,
+        name: 'Languages',
+        skill: ['C', 'C++', 'Python', 'JavaScript', 'HTML', 'CSS'],
+    },
+    {
+        id: 2,
+        name: 'IT Constructs',
+        skill: ['Data Structures and Algorithms', 'Operating Systems', 'Database Management Systems', 'Computer Networks', 'Machine Learning'],
+    },
+    {
+        id: 3,
+        name: 'Frameworks',
+        skill: ['NodeJs', 'Express', 'MySQL', 'MongoDB', 'React', 'TensorFlow', 'Keras', 'Django', 'Flask', 'Git'],
+    },
+    {
+        id: 4,
+        name: 'Database Languages',
+        skill: ['SQL', 'NoSQL'],
+    },
+]
+
+export default skills;
