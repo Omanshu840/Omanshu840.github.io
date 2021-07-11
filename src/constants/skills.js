07b13a252e3e886ef9800fcb27dd87cbc00c6f8e
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-07-11 22:32:14 +0530

    Updated Skills

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -2,22 +2,22 @@ const skills = [
     {
         id: 1,
         name: 'Languages',
-        skill: ['C', 'C++', 'Python', 'JavaScript', 'HTML', 'CSS'],
+        skill: ['C', 'C++', 'Python', 'Java', 'JavaScript', 'HTML', 'CSS'],
     },
     {
         id: 2,
         name: 'IT Constructs',
-        skill: ['Data Structures and Algorithms', 'Operating Systems', 'Database Management Systems', 'Computer Networks', 'Machine Learning'],
+        skill: ['Data Structures and Algorithms', 'Operating Systems', 'Database Management Systems', 'Machine Learning', 'Natural Language Processing', 'Computer Networks'],
     },
     {
         id: 3,
         name: 'Frameworks',
-        skill: ['NodeJs', 'Express', 'MySQL', 'MongoDB', 'React', 'TensorFlow', 'Keras', 'Django', 'Flask', 'Git'],
+        skill: ['React.js', 'Bootstrap', 'Node.js', 'Express', 'Springboot', 'Django', 'Flask', 'MySQL', 'PostgreSQL', 'MongoDB', 'Couchbase', 'TensorFlow', 'Keras', 'PyTorch', 'Git'],
     },
     {
         id: 4,
-        name: 'Database Languages',
-        skill: ['SQL', 'NoSQL'],
+        name: 'Problem Solving',
+        skill: ['360+ Problems on LeetCode', '1832 (Highest) rating on CodeChef', '5 star Problem solving badge on HackerRank'],
     },
 ]
 
