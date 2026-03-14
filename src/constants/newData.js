32385372d59016a4ba304ea0d17b50dbc7c9eec4
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2026-03-14 13:21:08 +0530

    sync code

---
+++ src/constants/newData.js
@@ -0,0 +1,12 @@
+const newData = [
+    {
+        "name": "Code-Collab",
+        "role": "Mentor",
+        "repo": "https://github.com/Omanshu840/Code-Collab",
+    },
+    {
+        "name": "NITK-Connect",
+        "role": "Co-Collaborator",
+        "repo": "https://github.com/WebClub-NITK/connect-client"
+    }
+]
